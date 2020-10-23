68e4329d35aaf62307e22482f60c200fb8b0964e
DESKTOP-BI3L0RQ\TomBomb <thomasdes533@gmail.com>
2020-10-23 08:25:11 -0700

    Adding empty service and component via cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayPostsComponent } from './display-posts/display-posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayPostsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/display-posts/display-posts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-posts',
+  templateUrl: './display-posts.component.html',
+  styleUrls: ['./display-posts.component.scss']
+})
+export class DisplayPostsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
